b4821b1bd72fefcfc7f6fbe9e30b83c5d31ff0b5
Marijn Goedegebure <marijngoedegebure@gmail.com>
2013-06-08 22:15:40 +0200

    Fixed the Character Controll.

--- SideScroller/New Unity Project 2/Assets/NewScripts/CharacterControl/CharacterControl.cs
+++ SideScroller/New Unity Project 2/Assets/NewScripts/CharacterControl/CharacterControl.cs
@@ -17,8 +17,8 @@ public class CharacterControl : MonoBehaviour {
 	public GameObject camera;
 	
 	private bool jumping = false;
-	private bool allowedToMoveRight = true;
-	private bool allowedToMoveLeft = true;
+	public bool allowedToMoveRight = true;
+	public bool allowedToMoveLeft = true;
 	
 	public float timeBetweenJumps = 2.4f;
 	private float timeSinceLastJump = 0f;
@@ -26,7 +26,7 @@ public class CharacterControl : MonoBehaviour {
 	public GameObject meleeattack;
 	public GameObject platform;
 	
-	private Inventory inventory = new Inventory();
+	public GameObject inventory;
 	
 	private bool facingLeft = false;
 	private bool facingRight = true;
@@ -80,12 +80,12 @@ public class CharacterControl : MonoBehaviour {
 		
 		if (Input.GetKeyDown (KeyCode.A)) {
 			// Cycle through melee weapons
-			inventory.nextMeleeWeapon();
+			//inventory.nextMeleeWeapon();
 		}
 		
 		if (Input.GetKeyDown (KeyCode.S)) {
 			// Cycle through ranged weapons
-			inventory.nextRangedWeapon();
+			//inventory.nextRangedWeapon();
 		}
 		
 		// Possibilities:
@@ -126,24 +126,35 @@ public class CharacterControl : MonoBehaviour {
 	
 	private void checkMovement() {
 		// Check if your allowed to move left/right (so you cannot jump against walls and stay there
-		Vector3 sphereLocLeft = new Vector3(transform.position.x-0.5f,transform.position.y,transform.position.z);
-		check (sphereLocLeft);
-		
-		Vector3 sphereLocRight = new Vector3(transform.position.x+0.5f,transform.position.y,transform.position.z);
-		check (sphereLocRight);
+		Vector3 sphereLocForward = Vector3.zero;
+		if(facingLeft) {
+			sphereLocForward = new Vector3(transform.position.x-0.5f,transform.position.y,transform.position.z);
+		}
+		if(facingRight) {
+			sphereLocForward = new Vector3(transform.position.x+0.5f,transform.position.y,transform.position.z);
+		}
+		check (sphereLocForward);
 	}
 	
 	private void check(Vector3 sphereLoc) {
 		Collider[] colliders = Physics.OverlapSphere(sphereLoc,0.2f);
+		bool didSomething = false;
 		for(int i = 0;i < colliders.Length;i++) {
 			if(colliders[i].name == "Platform") {
-				if(colliders[i].transform.position.x > transform.position.x) {
+				if(facingRight) {
 					allowedToMoveRight = false;
+					allowedToMoveLeft = true;
 				}
-				if(colliders[i].transform.position.x < transform.position.x) {
+				if(facingLeft) {
+					allowedToMoveRight = true;
 					allowedToMoveLeft = false;
 				}
+				didSomething = true;
 			}
+		}
+		if( !didSomething) {
+			allowedToMoveRight = true;
+			allowedToMoveLeft = true;
 		}
 	}
 	
